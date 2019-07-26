0defd30f09a6b5494f1f888168dbf860c948333b
Kuralamudhan Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
2019-07-26 12:44:04 -0700

    adding inital packages for bootstrap cluster
    Change-Id: Ie8020c6669f709a90608bedf5146e1216bc01070

--- env/02_configure.sh
+++ env/02_configure.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
 set -xe
+
+source lib/logging.sh
+source lib/common.sh
+
+if [[ $EUID -ne 0 ]]; then
+    echo "confgiure script must be run as root"
+    exit 1
+fi
+
+function configure_kubelet() {
+	swapoff -a
+	#Todo addition kubelet configuration
+}
+
+function configure_kubeadm() {
+	#Todo error handing
+	kubeadm config images pull --kubernetes-version=$KUBE_VERSION
+}
+
+function configure_podman() {
+	#Todo later to change the CNI networking for podman networking
+}
