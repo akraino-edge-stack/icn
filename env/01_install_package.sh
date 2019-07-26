1caf837d89171867f3d727b7368ad7ddea689f1a
Kuralamudhan Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
2019-07-26 12:44:04 -0700

    adding inital packages for bootstrap cluster
    Change-Id: Ie8020c6669f709a90608bedf5146e1216bc01070

--- env/01_install_package.sh
+++ env/01_install_package.sh
@@ -1,4 +1,83 @@
 #!/usr/bin/env bash
 set -ex
 
+lib/common.sh
+lib/logging.sh
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+function install_essential_packages() {
+    apt-get update
+    apt-get -y install \
+		crudini \
+		curl \
+		dnsmasq \
+		figlet \
+		golang \
+		nmap \
+		patch \
+		psmisc \
+		python-pip \
+		python-requests \
+		python-setuptools \
+		vim \
+		wget
+}
+
+function install_ironic_packages() {
+    apt-get update
+    apt-get -y install \
+		jq \
+		nodejs \
+		python-ironicclient \
+		python-ironic-inspector-client \
+		python-lxml \
+		python-netaddr \
+		python-openstackclient \
+		unzip \
+		genisoimage
+    pip install \
+		lolcat \
+		yq
+}
+
+function install_docker_packages() {
+    apt-get remove docker \
+		docker-engine \
+		docker.io \
+		containerd \
+		runc
+    apt-get update
+    apt-get -y install \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository \
+		"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+		$(lsb_release -cs) \
+		stable"
+    apt-get update
+    apt-get -y install docker-ce=18.06.0~ce~3-0~ubuntu
+}
+
+function install_podman_packages() {
+    apt-get update
+    add-apt-repository -y ppa:projectatomic/ppa
+    apt-get -y install podman
+}
+
+function install_kubernetes_packages() {
+   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+   bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+   apt-get update
+   apt-get install -y kubelet=1.15.0-00 kubeadm=1.15.0-00 kubectl=1.15.0-00
+   apt-mark hold kubelet kubeadm kubectl
+}
---
+++ env/lib/common.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#supported OS version
+UBUNTU_BIONIC=${UBUNTU_BIONIC:-Ubuntu 18.04.2 LTS}
+
+#local apt repo
+LOCAL_APT_REPO=${LOCAL_APT_REPO:-$PWD/apt}
+PIP_CACHE_DIR=${PIP_CACHE_DIR:-$PWD/pip-cache-dir}
+BUILD_DIR=${BUILD_DIR:-$PWD/build-dir}
+
+#set variables
+#Todo include over all variables here
+KUBE_VERSION=${KUBE_VERSION:-"v1.15.0"}
+POD_NETWORK_CIDR=${POD_NETWORK_CIDR:-"10.244.0.0/16"}
+PODMAN_CNI_CONFLIST=${PODMAN_CNI_CONFLIST:-"https://raw.githubusercontent.com/containers/libpod/v1.4.4/cni/87-podman-bridge.conflist"}
+
+#Ironic variables
+IRONIC_IMAGE=${IRONIC_IMAGE:-"quay.io/metal3-io/ironic:master"}
+IRONIC_INSPECTOR_IMAGE=${IRONIC_INSPECTOR_IMAGE:-"quay.io/metal3-io/ironic-inspector"}
+IRONIC_DATA_DIR=${IRONIC_DATA_DIR:-"/opt/ironic"}
+IRONIC_PROVISIONING_INTERFACE=${IRONIC_PROVISIONING_INTERFACE:-"provisioning"}
+IRONIC_IPMI_INTERFACE=${IRONIC_IPMI_INTERFACE:-"eno1"}
+IRONIC_PROVISIONING_INTERFACE_IP=${IRONIC_PROVISIONING_INTERFACE_IP:-"172.22.0.1"}
+IRONIC_IPMI_INTERFACE_IP=${IRONIC_IPMI_INTERFACE_IP:-"172.31.1.9"}
+BM_IMAGE_URL=${BM_IMAGE_URL:-"https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"}
+BM_IMAGE=${BM_IMAGE:-"bionic-server-cloudimg-amd64.img"}
