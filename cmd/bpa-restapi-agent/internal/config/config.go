0238f9b8424354c5cdce2445163d8babd55116b4
enyinna1234 <enyinna.ochulor@intel.com>
2019-07-30 09:50:34 -0700

    Adding Initial ICN API Service
    This adds create for adding images to the db, read for listing
    images, delete for removing images from the database, and update
    for changing image description.
    
    Signed-off-by: Enyinna Ochulor <enyinna.ochulor@intel.com>
    Change-Id: I8f524c2b5cfef6bc7749b2dc272574c937f24a03

---
+++ cmd/bpa-restapi-agent/internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+  "encoding/json"
+  "log"
+  "os"
+)
+
+type Configuration struct {
+  Password        string  `json:  "password"`
+  DatabaseAddress string  `json:  "database-address"`
+  DatabaseType    string  `json:  "database-type"`
+  ServicePort     string  `json:  "service-port"`
+}
+
+var gConfig *Configuration
+
+func readConfigFile(file string) (*Configuration, error) {
+  f, err := os.Open(file)
+  if err != nil {
+    return defaultConfiguration(), err
+  }
+  defer f.Close()
+
+  conf := defaultConfiguration()
+
+  decoder := json.NewDecoder(f)
+  err = decoder.Decode(conf)
+  if err != nil {
+    return conf, err
+  }
+
+  return conf, nil
+}
+
+func defaultConfiguration() *Configuration {
+  return &Configuration {
+    Password:           "",
+    DatabaseAddress:    "127.0.0.1",
+    DatabaseType:       "mongo",
+    ServicePort:        "9015",
+  }
+}
+
+func GetConfiguration() *Configuration {
+  if gConfig == nil {
+    conf, err := readConfigFile("ICNconfig.json")
+    if err != nil {
+      log.Println("Error loading config file. Using defaults")
+    }
+
+    gConfig = conf
+  }
+
+  return gConfig
+}
