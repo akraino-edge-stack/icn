f03e4c15bf7c5e70acbb0ffae845782021527d42
Chen, Tingjie <tingjie.chen@intel.com>
2019-07-30 08:43:08 +0000

    Add Rook operator and Ceph cluster bring up script
    By default create osd on folder /rook/storage-dir with 10GB for database
    and journal.
    
    Change-Id: I7f296701ac1126852fee6b0c57b79c2e53455da2
    Signed-off-by: Chen, Tingjie <tingjie.chen@intel.com>

---
+++ deploy/kud-plugin-addons/rook/README.md
@@ -0,0 +1,8 @@
+# Intel Rook infrastructure for Ceph cluster deployment
+
+# By default create osd on folder /rook/storage-dir with 10GB for database
+# and journal.
+
+cd yaml
+./install.sh
+
---
+++ deploy/kud-plugin-addons/rook/yaml/collect_rook_yaml.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# usage: collect_rook_yaml.sh [target]
+
+set -ex
+
+if [ $# -ne 1 ] ; then
+    echo "Please input the target folder!"
+    exit 0
+fi
+
+VER="0.1"
+MKDIR_P="mkdir -p"
+target=$1
+temp=rook_yaml
+
+# copy to target
+$MKDIR_P $temp
+cp rook-common.yaml $temp/
+cp rook-operator-with-csi.yaml $temp/
+cp rook-ceph-cluster.yaml $temp/
+cp rook-toolbox.yaml $temp/
+cp -rf ./csi/ $temp/
+cp install.sh $temp/
+
+if [ ! -d $target/yaml ]; then
+    $MKDIR_P $target/yaml;
+fi;
+
+tar czvf $target/yaml/rook_yaml-$VER.tar.gz $temp/
+
+# clear
+rm -rf $temp
