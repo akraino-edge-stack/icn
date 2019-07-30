2fb7d2488bba1f9ceafb87dbd9f472473224fafd
enyinna1234 <enyinna.ochulor@intel.com>
2019-07-30 09:50:34 -0700

    Adding Initial ICN API Service
    This adds create for adding images to the db, read for listing
    images, and delete for removing images from the database.
    
    Signed-off-by: Enyinna Ochulor <enyinna.ochulor@intel.com>
    Change-Id: I8f524c2b5cfef6bc7749b2dc272574c937f24a03

---
+++ cmd/bpa-restapi-agent/api/api.go
@@ -0,0 +1,54 @@
+// api/api_images.go
+
+
+package api
+
+import (
+	image "bpa-restapi-agent/internal/app"
+
+	"github.com/gorilla/mux"
+)
+
+// To Do - Add connection
+
+// NewRouter creates a router that registers the various urls that are supported
+func NewRouter(binaryClient image.ImageManager,
+							 containerClient image.ImageManager,
+							 osClient image.ImageManager) *mux.Router {
+
+	router := mux.NewRouter()
+
+	//Setup the image install api handler here
+	if binaryClient == nil {
+		binaryClient = image.NewBinaryImageClient()
+	}
+	binaryHandler := imageHandler{client: binaryClient}
+	imgRouter := router.PathPrefix("/v1").Subrouter()
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/binary_images", binaryHandler.createHandler).Methods("POST")
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/binary_images/{imgname}", binaryHandler.getHandler).Methods("GET")
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/binary_images/{imgname}", binaryHandler.deleteHandler).Methods("DELETE")
+
+	//Setup the _image install api handler here
+	if containerClient == nil {
+		containerClient = image.NewContainerImageClient()
+	}
+	containerHandler := imageHandler{client: containerClient}
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/container_images", containerHandler.createHandler).Methods("POST")
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/container_images/{imgname}", containerHandler.getHandler).Methods("GET")
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/container_images/{imgname}", containerHandler.deleteHandler).Methods("DELETE")
+
+	//Setup the os_image install api handler here
+	if osClient == nil {
+		osClient = image.NewOSImageClient()
+	}
+	osHandler := imageHandler{client: osClient}
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/os_images", osHandler.createHandler).Methods("POST")
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/os_images/{imgname}", osHandler.getHandler).Methods("GET")
+	imgRouter.HandleFunc("/baremetalcluster/{owner}/{clustername}/os_images/{imgname}", osHandler.deleteHandler).Methods("DELETE")
+
+
+	// Add healthcheck path
+	//instRouter.HandleFunc("/healthcheck", healthCheckHandler).Methods("GET")
+
+	return router
+}
---
+++ cmd/bpa-restapi-agent/main.go
@@ -0,0 +1,71 @@
+// main.go
+package main
+
+import (
+  "context"
+  "log"
+  "math/rand"
+  "net/http"
+  "os"
+  "os/signal"
+  "time"
+
+  //To Do - Implement internal for checking config
+  "github.com/gorilla/handlers"
+
+  "bpa-restapi-agent/api"
+  utils "bpa-restapi-agent/internal"
+  "bpa-restapi-agent/internal/config"
+)
+
+// func main() {
+//
+//   log.Printf("Starting Integrated Cloud Native API")
+//   router := api.NewRouter()
+//   //CORS for browser testing (ICN_delete)
+//   allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+//   allowedMethods := handlers.AllowedMethods([]string{"GET", "POST",
+//     "DELETE", "PUT"})
+//
+//   //log.Fatal(http.ListenAndServe(":8080", router))
+//
+//
+//   // launch server with CORS validations (ICN_delete)
+//   log.Fatal(http.ListenAndServe(":9000",
+//     handlers.CORS(allowedOrigins, allowedMethods) (router)))
+// }
+
+func main() {
+  // To Do - Implement initial settings
+  // check initial config
+  err := utils.CheckInitialSettings()
+  if err != nil{
+    log.Fatal(err)
+  }
+
+  rand.Seed(time.Now().UnixNano())
+
+  httpRouter := api.NewRouter(nil, nil, nil)
+  // Return http.handler and log requests to Stdout
+  loggedRouter := handlers.LoggingHandler(os.Stdout, httpRouter)
+  log.Println("Starting Integrated Cloud Native API")
+
+  // Create custom http server
+  httpServer := &http.Server{
+    Handler: loggedRouter,
+    // To Do - Implement config
+    Addr:    ":" + config.GetConfiguration().ServicePort,
+  }
+  connectionsClose := make(chan struct{})
+  go func() {
+    c := make(chan os.Signal, 1) // create c channel to receive notifications
+    signal.Notify(c, os.Interrupt) // register c channel to run concurrently
+    <-c
+    httpServer.Shutdown(context.Background())
+    close(connectionsClose)
+  }()
+
+  // Start server
+  log.Fatal(httpServer.ListenAndServe())
+
+}
