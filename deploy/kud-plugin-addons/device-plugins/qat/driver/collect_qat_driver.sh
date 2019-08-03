ab68b085d6410a9c9dc7e524081349953817f802
hle2 <huifeng.le@intel.com>
2019-08-03 09:32:04 +0800

    Add SRIOV and QAT device plugin deploy components
    Add deploy components include (1) SRIOV/QAT libraries (2) device plugin
    yaml for SRIOV/QAT device plugin.
    
    Change-Id: I1ae395d4d51d187d8fc5cb8f7a4d5b5e50ea8433

---
+++ deploy/kud-plugin-addons/device-plugins/qat/driver/collect_qat_driver.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# usage: collect_qat_driver.sh [target]
+
+set -ex
+
+if [ $# -ne 1 ] ; then
+    echo "Please input the target folder!"
+    exit 0
+fi
+
+VER="0.1"
+MKDIR_P="mkdir -p"
+target=$1
+package=qat1.7.l.4.6.0-00025
+
+# download driver source package
+if [ ! -e /tmp/$package.tar.gz ]; then
+    wget -P /tmp https://01.org/sites/default/files/downloads/$package.tar.gz
+fi
+cp /tmp/$package.tar.gz .
+
+# compile
+$MKDIR_P $package
+tar xzvf $package.tar.gz -C $package
+pushd `pwd`
+cd $package
+./configure --enable-icp-sriov=host
+make
+popd
+
+# copy to target
+$MKDIR_P qat_driver
+cp -r $package/build/* qat_driver/
+cp install_qat.sh qat_driver/install.sh
+cp qat qat_driver/
+
+if [ ! -d $target/driver ]; then
+    $MKDIR_P $target/driver;
+fi;
+
+tar czvf $target/driver/qat_driver-$VER.tar.gz qat_driver/
+
+# clear
+rm -rf $package
+rm -rf qat_driver
+rm $package.tar.gz
