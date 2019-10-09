5d872d38566c55f3c958bd672b341ad7efb238cf
enyinna1234 <enyinna.ochulor@intel.com>
2019-10-09 10:18:22 -0700

    Adds unit tests and K8s artifacts
    This adds unit tests. It also adds a Kubernetes Service, and
    Deployment to create objects that enable the RESTful API as
    a Kubenetes service.
    
    Signed-off-by: Enyinna Ochulor <enyinna.ochulor@intel.com>
    Change-Id: I0439ff51931ac0abe7c5bff8b7d75e8ced19a06f

--- cmd/bpa-restapi-agent/internal/app/image.go
+++ cmd/bpa-restapi-agent/internal/app/image.go
@@ -16,9 +16,9 @@ type Image struct {
 	ClusterName         string               `json:"cluster_name"`
 	Type                string               `json:"type"`
 	ImageName           string               `json:"image_name"`
-	ImageOffset					*int							   `json:"image_offset"`
-	ImageLength					int							 		 `json:"image_length"`
-	UploadComplete			*bool								 `json:"upload_complete"`
+	ImageOffset			*int				 `json:"image_offset"`
+	ImageLength		    int					 `json:"image_length"`
+	UploadComplete		*bool			     `json:"upload_complete"`
 	Description         ImageRecordList      `json:"description"`
 }
 
@@ -28,8 +28,6 @@ type ImageRecordList struct {
 
 // ImageKey is the key structure that is used in the database
 type ImageKey struct {
-	// Owner            string     `json:"owner"`
-	// ClusterName      string     `json:"cluster_name"`
 	ImageName        string     `json:"image_name"`
 }
 
@@ -39,7 +37,7 @@ func (dk ImageKey) String() string {
 	out, err := json.Marshal(dk)
 	if err != nil {
 		return ""
-	}
+    }
 
 	return string(out)
 }
@@ -47,16 +45,31 @@ func (dk ImageKey) String() string {
 // ImageManager is an interface that exposes the Image functionality
 type ImageManager interface {
 	Create(c Image) (Image, error)
-	Get(imageName string) (Image, error)
+    Get(imageName string) (Image, error)
 	Delete(imageName string) error
 	Update(imageName string, c Image) (Image, error)
 	GetImageRecordByName(imgname, imageName string) (map[string]string, error)
 	GetDirPath(imageName string) (string, string, error)
+
 }
 
+// Interface to aid unit test by mocking third party packages
+type Utility interface {
+    GetCurrentUser() (*user.User, error)
+    DBCreate(storeName string, key ImageKey, meta string, c Image) error
+    DBRead(storeName string, key ImageKey, meta string) ([]byte, error)
+    DBUnmarshal(value []byte, c Image) error
+    OSMakeDir(dirpath string, perm int) error
+    OSCreateFile(filePath string) error
+    GetPath(currUser *user.User, imageName string) (string, string)
+    DBDelete(storeName string, key ImageKey, meta string) error
+    OSRemove(filePath string) error
+    DBUpdate(key ImageKey, c Image) error
+}
 // ImageClient implements the ImageManager
 // It will also be used to maintain some localized state
 type ImageClient struct {
+    util Utility
 	storeName string
 	tagMeta   string
 }
@@ -87,13 +100,10 @@ func NewOSImageClient() *ImageClient {
 
 // Create an entry for the Image resource in the database`
 func (v *ImageClient) Create(c Image) (Image, error) {
-
-	//Construct composite key consisting of name
-	key := ImageKey{
-		// Owner:	c.Owner,
-		// ClusterName:	c.ClusterName,
-		ImageName: c.ImageName,
-	}
+    //Construct composite key consisting of name
+    key := ImageKey{
+        ImageName: c.ImageName,
+    }
 
 	//Check if this Image already exists
 	_, err := v.Get(c.ImageName)
@@ -101,12 +111,12 @@ func (v *ImageClient) Create(c Image) (Image, error) {
 		return Image{}, pkgerrors.New("Image already exists")
 	}
 
-	err = db.DBconn.Create(v.storeName, key, v.tagMeta, c)
+	err = v.util.DBCreate(v.storeName, key, v.tagMeta, c)
 	if err != nil {
 		return Image{}, pkgerrors.Wrap(err, "Creating DB Entry")
 	}
 
-	err = v.CreateFile(v.storeName, c)
+	err = v.CreateFile(c)
 	if err != nil {
 		return Image{}, pkgerrors.Wrap(err, "Creating File in FS")
 	}
@@ -114,28 +124,53 @@ func (v *ImageClient) Create(c Image) (Image, error) {
 	return c, nil
 }
 
+func (v *ImageClient) DBCreate(storeName string, key ImageKey, meta string, c Image) error {
+
+    //Construct composite key consisting of name
+    err  := db.DBconn.Create(storeName, key, meta, c)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Creating DB Entry")
+    }
+    
+    return nil
+}
+
 // Create file
 
-func (v *ImageClient) CreateFile(dirName string, c Image) error {
+func (v *ImageClient) CreateFile(c Image) error {
 	filePath, dirPath, err := v.GetDirPath(c.ImageName)
 	if err != nil {
 		return pkgerrors.Wrap(err, "Get file path")
 	}
-  err = os.MkdirAll(dirPath, 0744)
-  if err != nil {
-		return pkgerrors.Wrap(err, "Make image directory")
-  }
-	file1, err := os.Create(filePath)
+    err = v.util.OSMakeDir(dirPath, 0744)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Make image directory")
+    }
+	err = v.util.OSCreateFile(filePath)
 	if err != nil {
 		return pkgerrors.Wrap(err, "Create image file")
 	}
-
-	defer file1.Close()
-
-
-  return nil
+    
+    return nil
 }
 
+func (v *ImageClient) OSMakeDir(dirPath string, perm int) error {
+    err := os.MkdirAll(dirPath, 0744)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Make image directory")
+    }
+    return nil
+}
+
+func (v *ImageClient) OSCreateFile(filePath string) error {
+    file1, err := os.Create(filePath)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Create image file")
+    }
+    defer file1.Close()
+
+    return nil
+}
 
 // Get returns Image for corresponding to name
 func (v *ImageClient) Get(imageName string) (Image, error) {
@@ -147,7 +182,7 @@ func (v *ImageClient) Get(imageName string) (Image, error) {
 		ImageName: imageName,
 	}
 
-	value, err := db.DBconn.Read(v.storeName, key, v.tagMeta)
+	value, err := v.util.DBRead(v.storeName, key, v.tagMeta)
 	if err != nil {
 		return Image{}, pkgerrors.Wrap(err, "Get Image")
 	}
@@ -155,7 +190,7 @@ func (v *ImageClient) Get(imageName string) (Image, error) {
 	//value is a byte array
 	if value != nil {
 		c := Image{}
-		err = db.DBconn.Unmarshal(value, &c)
+		err = v.util.DBUnmarshal(value, c)
 		if err != nil {
 			return Image{}, pkgerrors.Wrap(err, "Unmarshaling Value")
 		}
@@ -163,6 +198,24 @@ func (v *ImageClient) Get(imageName string) (Image, error) {
 	}
 
 	return Image{}, pkgerrors.New("Error getting Connection")
+}
+
+func (v *ImageClient) DBRead(storeName string, key ImageKey, meta string)([]byte, error) {
+    value, err := db.DBconn.Read(storeName, key, meta)
+    if err != nil {
+        return []byte{}, pkgerrors.Wrap(err, "Get Image")
+    }
+
+    return value, nil
+}
+
+func (v *ImageClient) DBUnmarshal(value []byte, c Image) error {
+    err := db.DBconn.Unmarshal(value, &c)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Unmarshaling Value")
+    }
+
+    return nil
 }
 
 func (v *ImageClient) GetImageRecordByName(imgName string,
@@ -183,13 +236,8 @@ func (v *ImageClient) GetImageRecordByName(imgName string,
 }
 
 func (v *ImageClient) GetDirPath(imageName string) (string, string, error) {
-	u, err := user.Current()
-	if err != nil {
-		return "", "", pkgerrors.Wrap(err, "Current user")
-	}
-	home := u.HomeDir
-	dirPath := path.Join(home, "images", v.storeName)
-	filePath := path.Join(dirPath, imageName)
+    u, err := v.util.GetCurrentUser()
+    filePath, dirPath := v.util.GetPath(u, imageName)
 
 	return filePath, dirPath, err
 }
@@ -203,21 +251,34 @@ func (v *ImageClient) Delete(imageName string) error {
 		// ClusterName:	clusterName,
 		ImageName: imageName,
 	}
-	err := db.DBconn.Delete(v.storeName, key, v.tagMeta)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Delete Image")
-	}
+	err := v.util.DBDelete(v.storeName, key, v.tagMeta)
 
 	//Delete image from FS
 	filePath, _, err := v.GetDirPath(imageName)
 	if err != nil {
 		return pkgerrors.Wrap(err, "Get file path")
 	}
-	err = os.Remove(filePath)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Delete image file")
-	}
+	err = v.util.OSRemove(filePath)
+
 	return nil
+}
+
+func (v *ImageClient) OSRemove(filePath string) error {
+    err := os.Remove(filePath)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Delete image file")
+    }
+
+    return nil
+}
+
+func (v *ImageClient) DBDelete(storeName string, key ImageKey, meta string) error {
+    err := db.DBconn.Delete(v.storeName, key, v.tagMeta)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Delete Image")
+    }
+
+    return nil
 }
 
 // Update an entry for the image in the database
@@ -233,10 +294,34 @@ func (v *ImageClient) Update(imageName string, c Image) (Image, error) {
 		return Image{}, pkgerrors.New("Update Error - Image doesn't exist")
 	}
 
-	err = db.DBconn.Update(v.storeName, key, v.tagMeta, c)
-	if err != nil {
-		return Image{}, pkgerrors.Wrap(err, "Updating DB Entry")
-	}
+	err = v.util.DBUpdate(key, c)
 
 	return c, nil
 }
+
+func (v *ImageClient) DBUpdate(key ImageKey, c Image) error {
+    err := db.DBconn.Update(v.storeName, key, v.tagMeta, c)
+    if err != nil {
+        return pkgerrors.Wrap(err, "Updating DB Entry")
+    }
+
+    return nil
+}
+
+// Define GetCurrentUser
+func (v *ImageClient) GetCurrentUser() (*user.User, error) { 
+    u, err := user.Current()
+    if err != nil {
+        return nil, pkgerrors.Wrap(err, "Current user")
+    }
+
+    return u, nil
+}
+
+func (v *ImageClient) GetPath(currUser *user.User, imageName string) (string, string) {
+    home := currUser.HomeDir
+    dirPath := path.Join(home, "images", v.storeName)
+    filePath := path.Join(dirPath, imageName)
+
+    return filePath, dirPath
+} 
---
+++ cmd/bpa-restapi-agent/internal/app/image_test.go
@@ -0,0 +1,164 @@
+package app
+
+import (
+  "fmt"
+  "os/user"
+  "testing"
+
+  "github.com/stretchr/testify/mock"
+  "github.com/pkg/errors"
+
+)
+
+type mockValues struct {
+  mock.Mock
+}
+
+func (m *mockValues) GetCurrentUser() (*user.User, error) {
+  fmt.Println("Mocked Get User")
+  args := m.Called()
+
+  return args.Get(0).(*user.User), args.Error(1)
+}
+
+func TestGetDirPath(t *testing.T) {
+  fakeUser := user.User{}
+  u := &fakeUser
+
+  myMocks := new(mockValues)
+
+  myMocks.On("GetCurrentUser").Return(&fakeUser, nil)
+  myMocks.On("GetPath", u, "").Return("", "")
+
+  imageClient := ImageClient{myMocks, "test_image", "test_meta"}
+  _, dir, err := imageClient.GetDirPath("")
+  if err != nil {
+    t.Errorf("Path was incorrect, got: %q, want: %q.", dir, "some_path")
+  }
+}
+
+func (m *mockValues) DBCreate(name string, key ImageKey, meta string, c Image) error{
+    fmt.Println("Mocked Create image in Mongo")
+    args := m.Called(name, key, meta, c)
+
+    return args.Error(0)
+}
+
+func (m *mockValues) DBRead(name string, key ImageKey, meta string) ([]byte, error) {
+    fmt.Println("Mocked Mongo DB Read Operations")
+    args := m.Called(name, key, meta)
+
+    return args.Get(0).([]byte), args.Error(1)
+}
+
+func (m *mockValues) DBUnmarshal(value []byte, c Image) error {
+    fmt.Println("Mocked Mongo DB Unmarshal Operation")
+    args := m.Called(value, c)
+
+    return args.Error(0)
+}
+
+func (m *mockValues) GetPath(u *user.User, imageName string) (string, string) {
+    args := m.Called(u, "")
+
+    return args.String(0), args.String(1)
+}
+
+func (m *mockValues) OSMakeDir(dirPath string, perm int) error {
+    fmt.Println("Mocked OS Create Directory Operation")
+    args := m.Called(dirPath, perm)
+    
+    return args.Error(0)
+}
+
+func (m *mockValues) OSCreateFile(filePath string) error {
+    fmt.Println("Mocked Create File Operation")
+    args := m.Called(filePath)
+
+    return args.Error(0)
+}
+
+func (m *mockValues) DBDelete(name string, key ImageKey, meta string) error {
+    fmt.Println("Mocked Mongo DB Delete")
+    args := m.Called(name, key, meta)
+
+    return args.Error(0)
+
+}
+
+func (m *mockValues) OSRemove(filePath string) error {
+    fmt.Println("Mocked OS File Remove")
+    args := m.Called(filePath)
+
+    return args.Error(0)
+}
+
+func (m *mockValues) DBUpdate(k ImageKey, c Image) error {
+    fmt.Println("Mocked Mongo DB Update")
+    args := m.Called(k, c)
+
+    return args.Error(0)
+}
+
+func TestCreate(t *testing.T) {
+    image := Image{}
+    arr_data := []byte{}
+    key := ImageKey{}
+    myMocks := new(mockValues)
+    // just to get an error value
+    err1 := errors.New("math: square root of negative number")
+
+    fakeUser := user.User{}
+    u := &fakeUser
+
+    myMocks.On("DBCreate", "test_image", key, "test_meta", image).Return(nil)
+    myMocks.On("DBRead", "test_image", key, "test_meta").Return(arr_data, err1)
+    myMocks.On("DBUnmarshal", arr_data, image).Return(nil)
+    myMocks.On("GetCurrentUser").Return(&fakeUser, nil)
+    myMocks.On("GetPath", u, "").Return("", "")
+    myMocks.On("OSMakeDir", "", 0744).Return(nil)
+    myMocks.On("OSCreateFile", "").Return(nil)
+
+    imageClient := ImageClient{myMocks, "test_image", "test_meta"}
+    _, err := imageClient.Create(image)
+    if err != nil {
+        t.Errorf("Some error occured!")
+    }
+}
+
+func TestDelete(t *testing.T) {
+    key := ImageKey{}
+    fakeUser := user.User{}
+    u := &fakeUser
+
+    myMocks := new(mockValues)
+
+    myMocks.On("DBDelete", "test_image", key, "test_meta").Return(nil)
+    myMocks.On("GetCurrentUser").Return(&fakeUser, nil)
+    myMocks.On("GetPath", u, "").Return("", "")
+    myMocks.On("OSRemove", "").Return(nil)
+
+    imageClient := ImageClient{myMocks, "test_image", "test_meta"}
+    err := imageClient.Delete("")
+    if err != nil {
+        t.Errorf("Some error occured!")
+    }
+
+}
+
+func TestUpdate(t *testing.T) {
+    image := Image{}
+    key := ImageKey{}
+    arr_data := []byte{} 
+
+    myMocks := new(mockValues)
+
+    myMocks.On("DBRead", "test_image", key, "test_meta").Return(arr_data, nil)
+    myMocks.On("DBUnmarshal", arr_data, image).Return(nil)
+    myMocks.On("DBUpdate", key, image).Return(nil)
+    imageClient := ImageClient{myMocks, "test_image", "test_meta"}
+    _, err := imageClient.Update("", image)
+    if err != nil {
+        t.Errorf("Some error occured!")
+    }
+}
