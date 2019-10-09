cf6111c22ef1eb6bcc2d7567ad117635ab9a31d3
enyinna1234 <enyinna.ochulor@intel.com>
2019-10-09 10:18:22 -0700

    Adds unit tests and K8s artifacts
    This adds unit tests, a Kubernetes Service, and
    Deployment to create objects that enable the RESTful API as
    a Kubenetes service, and adds end to end testing.
    
    Change-Id: I0439ff51931ac0abe7c5bff8b7d75e8ced19a06f
    Signed-off-by: Enyinna Ochulor <enyinna.ochulor@intel.com>
    Signed-off-by: Chen, Tingjie <tingjie.chen@intel.com>

---
+++ cmd/bpa-restapi-agent/e2e_test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ICN_DIR=$(dirname "$(dirname "$PWD")")
+
+source "$ICN_DIR/env/lib/common.sh"
+
+#create sample image
+if true ; then
+	cat <<- EOF > /tmp/sample_image
+	This is a dummy file for testing.
+	EOF
+fi
+
+IMAGE_SIZE=$(ls -al /tmp/sample_image | awk '{print $5}')
+
+if true ; then
+	cat <<- EOF > /tmp/sample.json
+	{
+		"owner":  "alpha",
+		"cluster_name": "beta",
+		"type": "container",
+		"image_name": "qwerty123",
+		"image_length": $IMAGE_SIZE,
+		"image_offset": 0,
+		"upload_complete":  false,
+		"description": {
+			"image_records":  [
+				{
+					"image_record_name": "iuysdi1234",
+					"repo": "icn",
+					"tag":  "2"
+				}
+			]
+		}
+	}
+	EOF
+fi
+
+sleep 15
+
+kubectl get pods --all-namespaces
+
+ls -al /tmp
+
+#Get CLusterIP
+CLUSTER_IP=$(kubectl get services | grep bpa-api-service | awk '{print $3}')
+
+POST=$(call_api -i -F "metadata=</tmp/sample.json;type=application/json" -F file=@/tmp/sample.json -X POST http://$CLUSTER_IP:9015/v1/baremetalcluster/alpha/beta/container_images)
+
+
+GET=$(call_api -i -X GET http://$CLUSTER_IP:9015/v1/baremetalcluster/alpha/beta/container_images/qwerty123)
+
+PATCH=$(call_api --request PATCH --data-binary "@/tmp/sample_image" http://$CLUSTER_IP:9015/v1/baremetalcluster/alpha/beta/container_images/qwerty123 --header "Upload-Offset: 0" --header "Expect:" -i)
+
+DELETE=$(call_api -i -X DELETE http://$CLUSTER_IP:9015/v1/baremetalcluster/alpha/beta/container_images/qwerty123)
