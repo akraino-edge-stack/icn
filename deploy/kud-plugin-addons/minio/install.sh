845727ce647e027455837aecb6b9547865f3b679
enyinna1234 <enyinna.ochulor@intel.com>
2019-10-09 10:18:22 -0700

    Adds unit tests and K8s artifacts
    This adds unit tests, a Kubernetes Service, and
    Deployment to create objects that enable the RESTful API as
    a Kubenetes service, and adds end to end testing.
    
    Signed-off-by: Enyinna Ochulor <enyinna.ochulor@intel.com>
    Change-Id: I0439ff51931ac0abe7c5bff8b7d75e8ced19a06f

--- deploy/kud-plugin-addons/minio/install.sh
+++ deploy/kud-plugin-addons/minio/install.sh
@@ -5,17 +5,17 @@
 echo "s"|sudo -S mkdir /mnt/minio
 
 # Create local-storage persistent volume first since not support dynamic provisioning.
-kubectl create -f local-pv.yaml
+kubectl create -f ~/enyinna/icn/deploy/kud-plugin-addons/minio/local-pv.yaml
 
 # Create storage class for local-storage
-kubectl create -f local-sc.yaml
+kubectl create -f ~/enyinna/icn/deploy/kud-plugin-addons/minio/local-sc.yaml
 
 # Create persistent volume claim for minio server
-kubectl create -f local-pvc.yaml
+kubectl create -f ~/enyinna/icn/deploy/kud-plugin-addons/minio/local-pvc.yaml
 
 # Create deployment of MinIO server
-kubectl create -f minio-deployment.yaml
+kubectl create -f ~/enyinna/icn/deploy/kud-plugin-addons/minio/minio-deployment.yaml
 
 # Create service for MinIO
-kubectl create -f minio-service.yaml
+# kubectl create -f minio-service.yaml
 
