3bf201a72d83ea015b14ce0b1164dfd5d9007734
chengli3 <cheng1.li@intel.com>
2019-10-24 18:44:36 +0800

    Install private jenkins for CD jobs
    This patch is to create ansible playbook for jenkins installation.
    The private jenkins is for CD jobs. So it needs vagrant/libvirt to
    create VMs.
    
    Also this patch covers the jjb scripts.
    
    Signed-off-by: chengli3 <cheng1.li@intel.com>
    Change-Id: I5a2f050a269842aa9cb6258c0039ae2573553087

---
+++ ci/jjb/shell/org-logs-deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> logs-deploy.sh"
+
+# Ensure we fail the job if any steps fail
+# Disable 'globbing'
+set -euf -o pipefail
+
+JENKINS_HOSTNAME=pod14-node2
+NEXUS_URL=https://nexus.akraino.org
+LOGS_SERVER=https://logs.akraino.org
+
+NEXUS_LOG_SUBDIR=${NEXUS_LOG_SUBDIR:-$SILO}
+
+source ~/.local/bin/activate
+if [[ -z $"${LOGS_SERVER:-}" ]]; then
+    echo "WARNING: Logging server not set"
+else
+    nexus_url="${NEXUSPROXY:-$NEXUS_URL}"
+    nexus_path="${NEXUS_LOG_SUBDIR}/${JENKINS_HOSTNAME}/${JOB_NAME}/${BUILD_NUMBER}"
+
+    if [[ -n ${ARCHIVE_ARTIFACTS:-} ]] ; then
+        # Handle multiple search extensions as separate values to '-p|--pattern'
+        # "arg1 arg2" -> (-p arg1 -p arg2)
+        pattern_opts=()
+        for arg in $ARCHIVE_ARTIFACTS; do
+            pattern_opts+=("-p" "$arg")
+        done
+        lftools deploy archives "${pattern_opts[@]}" \
+                "$nexus_url" "$nexus_path" "$WORKSPACE"
+    else
+        lftools deploy archives "$nexus_url" "$nexus_path" "$WORKSPACE"
+    fi
+    lftools deploy logs "$nexus_url" "$nexus_path" "${BUILD_URL:-}"
+
+    echo "Build logs: <a href=\"$LOGS_SERVER/$nexus_path\">$LOGS_SERVER/$nexus_path</a>"
+fi
---
+++ ci/jjb/shell/verify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+echo "---> verify.sh"
+
+# Ensure we fail the job if any steps fail
+# Disable 'globbing'
+set -exuf -o pipefail
+
+function clean_vm {
+  vagrant destroy -f
+}
+trap clean_vm EXIT
+
+vagrant destroy -f
+vagrant up
+vagrant ssh -c "
+set -exuf
+sudo parted /dev/sda resizepart 3 yes 100%
+sudo resize2fs /dev/sda3
+sudo apt update
+sudo apt install -y make
+cd /vagrant
+sudo make verifier
+"
