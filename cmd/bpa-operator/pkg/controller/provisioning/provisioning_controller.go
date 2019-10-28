a061efb4df0f7fe1c5d2e2cbe319032970e6edc0
ramamani yeleswarapu <ramamani.yeleswarapu@intel.com>
2019-10-28 15:07:48 -0700

    Add e2e script for Virtlet VM provisioning
    - Adds e2e script, provisioning cr yaml for testing
      Virtlet VM provisioning.
    - Modifies Makefiles.
    - Modifies kud_bm_launch.sh.
    
    Signed-off-by: Ramamani Yeleswarapu <ramamani.yeleswarapu@intel.com>
    Change-Id: I6a9714b3a5c2b7a71566ce854a7b8d52e637bb17

--- cmd/bpa-operator/pkg/controller/provisioning/provisioning_controller.go
+++ cmd/bpa-operator/pkg/controller/provisioning/provisioning_controller.go
@@ -273,6 +273,9 @@ func (r *ReconcileProvisioning) Reconcile(request reconcile.Request) (reconcile.
 		       }
 
                        allString += masterLabel + "  ansible_ssh_host="  + hostIPaddress + " ansible_ssh_port=22" + "\n"
+                       if clusterType == "virtlet-vm" {
+                           allString = masterLabel + "  ansible_ssh_host="  + hostIPaddress + " ansible_ssh_port=22" + " ansible_ssh_user=root" + " ansible_ssh_pass=root" + "\n"
+                       }
                        masterString += masterLabel + "\n"
                        clusterData[masterTag + masterLabel] = hostIPaddress
 
@@ -353,8 +356,10 @@ func (r *ReconcileProvisioning) Reconcile(request reconcile.Request) (reconcile.
 					   }
                                            fmt.Printf("%s : %s \n", hostIPaddress, workerMAC)
 
-
                                            allString += workerLabel + "  ansible_ssh_host="  + hostIPaddress + " ansible_ssh_port=22" + "\n"
+                                           if clusterType == "virtlet-vm" {
+                                               allString = masterLabel + "  ansible_ssh_host="  + hostIPaddress + " ansible_ssh_port=22" + " ansible_ssh_user=root" + " ansible_ssh_pass=root" + "\n"
+                                           }
                                            workerString += workerLabel + "\n"
 					   clusterData[workerTag + workerLabel] = hostIPaddress
 
