d15be08c7fd25d8f7c9a503b5a9705f4d7e9b3b2
Akhila Kishore <akhila.kishore@intel.com>
2019-10-28 16:59:37 -0700

    Adding end to end script to verify KuD.
    This script pulls KuD enables testing and brings up
    the vagrant machine and test cases. Removing
    white space
    
    Signed-off-by: Akhila Kishore <akhila.kishore@intel.com>
    Change-Id: Iebc8fb3ecf81ed57c57b45bf3bd0f25ffc5e9fb9

---
+++ deploy/kud/kud_e2e_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function _get_kud_ {
+  echo " Download KuD project.. "
+  git clone "git clone "https://gerrit.onap.org/r/multicloud/k8s""
+  pushd $kud_folder/config
+  mv default.yml samples/ha.yml
+  mv samples/pdf.yml.aio ../config/default.yml
+  popd
+  pushd $kud_folder
+  ./setup.sh -p libvirt
+  #Changing the default value to enable test cases in KuD
+  sed -i -e 's/testing_enabled=${KUD_ENABLE_TESTS:-false}/testing_enabled=${KUD_ENABLE_TESTS:-true}/g' installer.sh
+  #bring up the VMs
+  sudo vagrant up && sudo vagrant up installer
+
+}
+
+#configuration values
+kud_folder=~/icn/k8s/kud/hosting_providers/vagrant
+kud_inventory_folder=$kud_folder/inventory
+
+_get_kud_
