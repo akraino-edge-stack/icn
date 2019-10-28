932130144ecfb7692057635e24d685c69cdd781d
ramamani yeleswarapu <ramamani.yeleswarapu@intel.com>
2019-10-28 15:07:48 -0700

    Add e2e script for Virtlet VM provisioning
    - Adds e2e script, provisioning cr yaml for testing
      Virtlet VM provisioning.
    - Modifies Makefiles.
    
    Signed-off-by: Ramamani Yeleswarapu <ramamani.yeleswarapu@intel.com>
    Change-Id: I6a9714b3a5c2b7a71566ce854a7b8d52e637bb17

---
+++ cmd/bpa-operator/e2etest/bpa_e2e_vm_test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+printf "\n\nStart Virtlet VM provisioning E2E test\n\n"
+
+status=$(kubectl get pods --all-namespaces -o wide)
+printf "\n$status\n\n"
+
+# create flannel-vm net-attach-def
+kubectl apply -f ../deploy/netattachdef-flannel-vm.yaml
+
+# create virtlet vm
+key=$(cat /root/.ssh/id_rsa.pub)
+cp ../deploy/virtlet-deployment-sample.yaml virtlet_test_vm.yaml
+sed -i "s|\$ssh_key|${key}|" virtlet_test_vm.yaml
+kubectl create -f virtlet_test_vm.yaml
+
+status=""
+while [[ $status != "Running" ]]
+do
+	printf "\nDescribe net-attach-def....\n"
+	val=$(kubectl describe net-attach-def)
+	printf "\n$val\n"
+
+	printf "\nDescribe virtlet-deployment pod....\n"
+	val=$(kubectl describe pods $(kubectl get pods |grep -i virtlet-deployment | awk '{print $1}'))
+	printf "\n$val\n"
+
+	stats=$(kubectl get pods |grep -i virtlet-deployment)
+	printf "\n$stats\n"
+	status=$(echo $stats | cut -d " " -f 3)
+	printf "\n$status\n"
+	if [[ $status == "Err"* ]]; then
+		echo "Error creating Virtlet VM, test incomplete"
+		kubectl delete -f virtlet_test_vm.yaml
+		exit 1
+	fi
+done
+
+sleep 3
+echo "Virtlet VM is ready for provisioning"
+
+# create the ssh secret before provisioning
+kubectl create secret generic ssh-key-secret --from-file=id_rsa=/root/.ssh/id_rsa \
+	--from-file=id_rsa.pub=/root/.ssh/id_rsa.pub
+
+# create provisioning cr
+kubectl apply -f e2e_vm_test_provisioning_cr.yaml
+
+status="Running"
+
+while [[ $status == "Running" ]]
+do
+	stats=$(kubectl get pods |grep -i kud-cluster-vm)
+	status=$(echo $stats | cut -d " " -f 3)
+	echo "KUD install job still running"
+	sleep 2m
+done
+
+if [[ $status == "Completed" ]]; then
+   printf "KUD Install completed successfully\n"
+else
+   printf "KUD Install failed\n"
+fi
+
+printf "\n\nBeginning E2E VM Test Teardown\n\n"
+
+kubectl delete -f provisioning_cr_test_vm.yaml
+kubectl delete job kud-cluster-vm
+kubectl delete -f virtlet_test_vm.yaml
