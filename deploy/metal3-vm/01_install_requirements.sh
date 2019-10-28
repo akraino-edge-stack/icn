e83aed9b0c462c69096cee14a26f75b3e52e024d
r.kuralamudhan <kuralamudhan.ramakrishnan@intel.com>
2019-10-27 22:33:56 -0700

    adding metal3 vm verifier
    Signed-off-by: r.kuralamudhan <kuralamudhan.ramakrishnan@intel.com>
    Change-Id: If76f121b30697e2d2b39df2f07942695250f4a86

---
+++ deploy/metal3-vm/01_install_requirements.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set +x
+
+OS=$(awk -F= '/^ID=/ { print $2 }' /etc/os-release | tr -d '"')
+if [[ $OS == ubuntu ]]; then
+  # shellcheck disable=SC1091
+  source ubuntu_install_requirements.sh
+else
+  # shellcheck disable=SC1091
+  source centos_install_requirements.sh
+fi
