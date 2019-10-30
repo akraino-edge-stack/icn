f5e12045aa879310153eac680c4e58b0989a2b7f
r.kuralamudhan <kuralamudhan.ramakrishnan@intel.com>
2019-10-30 06:15:21 +0000

    update metal3 scripts
    Signed-off-by: r.kuralamudhan <kuralamudhan.ramakrishnan@intel.com>
    Change-Id: I98254ae96bad3192cf6f9bd8214ad4766b6f3109

---
+++ deploy/metal3/scripts/01_metal3.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+set +ex
+
+LIBDIR="$(dirname "$(dirname "$(dirname "$PWD")")")"
+
+eval "$(go env)"
+
+source $LIBDIR/env/lib/common.sh
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+IMAGE_URL=http://172.22.0.1/images/${BM_IMAGE}
+IMAGE_CHECKSUM=http://172.22.0.1/images/${BM_IMAGE}.md5sum
+
+function get_default_inteface_ipaddress {
+    local _ip=$1
+    local _default_interface=$(awk '$2 == 00000000 { print $1 }' /proc/net/route)
+    local _ipv4address=$(ip addr show dev $_default_interface | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
+    eval $_ip="'$_ipv4address'"
+}
+
+function create_ssh_key {
+    #ssh key for compute node to communicate back to bootstrap server
+    mkdir -p $BUILD_DIR/ssh_key
+    ssh-keygen -C "compute.icn.akraino.lfedge.org" -f $BUILD_DIR/ssh_key/id_rsa
+    cat $BUILD_DIR/ssh_key/id_rsa.pub >> $HOME/.ssh/authorized_keys
+}
+
+function set_compute_key {
+    _SSH_LOCAL_KEY=$(cat $BUILD_DIR/ssh_key/id_rsa)
+    cat << EOF
+write_files:
+- path: /opt/ssh_id_rsa
+    owner: root:root
+    permissions: '0600'
+    content: |
+    $_SSH_LOCAL_KEY
+EOF
+}
+
+function deprovision_compute_node {
+    name="$1"
+    kubectl patch baremetalhost $name -n metal3 --type merge \
+    -p '{"spec":{"image":{"url":"","checksum":""}}}'
+}
+
+function set_compute_ssh_config {
+    get_default_inteface_ipaddress default_addr
+    cat << EOF
+- path: /root/.ssh/config
+    owner: root:root
+    permissions: '0600'
+    content: |
+    Host bootstrapmachine $default_addr
+    HostName $default_addr
+    IdentityFile /opt/ssh_id_rsa
+    User $USER
+- path: /etc/apt/sources.list
+    owner: root:root
+    permissions: '0665'
+    content: |
+    deb [trusted=yes] ssh://$USER@$default_addr:$LOCAL_APT_REPO ./
+EOF
+}
+
+function create_userdata {
+    name="$1"
+    COMPUTE_NODE_FQDN="$name.akraino.icn.org"
+    printf "#cloud-config\n" >  $name-userdata.yaml
+    if [ -n "$COMPUTE_NODE_PASSWORD" ]; then
+    printf "password: ""%s" "$COMPUTE_NODE_PASSWORD" >>  $name-userdata.yaml
+    printf "\nchpasswd: {expire: False}\n" >>  $name-userdata.yaml
+    printf "ssh_pwauth: True\n" >>  $name-userdata.yaml
+    fi
+
+    if [ -n "$COMPUTE_NODE_FQDN" ]; then
+    printf "fqdn: ""%s" "$COMPUTE_NODE_FQDN" >>  $name-userdata.yaml
+    printf "\n" >>  $name-userdata.yaml
+    fi
+    printf "disable_root: false\n" >>  $name-userdata.yaml
+    printf "ssh_authorized_keys:\n  - " >>  $name-userdata.yaml
+
+    if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+    yes y | ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa
+    fi
+
+    cat $HOME/.ssh/id_rsa.pub >>  $name-userdata.yaml
+    network_config_files >> $name-userdata.yaml
+    printf "\n" >>  $name-userdata.yaml
+}
+
+function launch_baremetal_operator {
+    if [ ! -d $GOPATH/src/github.com/metal3-io/baremetal-operator ]; then
+        go get github.com/metal3-io/baremetal-operator
+        git checkout 3d40caa29dce82878d83aeb7f8dab4dc4a856160
+    fi
+
+    pushd $GOPATH/src/github.com/metal3-io/baremetal-operator
+    make deploy
+    popd
+}
+
+function network_config_files {
+    cat << 'EOF'
+write_files:
+- path: /opt/ironic_net.sh
+  owner: root:root
+  permissions: '0777'
+  content: |
+    #!/usr/bin/env bash
+    set -xe
+    for intf in /sys/class/net/*; do
+        sudo ifconfig `basename $intf` up
+        sudo dhclient -nw `basename $intf`
+    done
+runcmd:
+ - [ /opt/ironic_net.sh ]
+EOF
+}
+
+function apply_userdata_credential {
+    name="$1"
+    cat <<EOF > ./$name-user-data-credential.yaml
+apiVersion: v1
+data:
+  userData: $(base64 -w 0 $name-userdata.yaml)
+kind: Secret
+metadata:
+  name: $name-user-data
+  namespace: metal3
+type: Opaque
+EOF
+    kubectl apply -n metal3 -f $name-user-data-credential.yaml
+}
+
+function make_bm_hosts {
+    while read -r name username password address; do
+        create_userdata $name
+        apply_userdata_credential $name
+
+        go run $GOPATH/src/github.com/metal3-io/baremetal-operator/cmd/make-bm-worker/main.go \
+           -address "ipmi://$address" \
+           -password "$password" \
+           -user "$username" \
+           "$name" > $name-bm-node.yaml
+
+        printf "  image:" >> $name-bm-node.yaml
+        printf "\n    url: ""%s" "$IMAGE_URL" >> $name-bm-node.yaml
+        printf "\n    checksum: ""%s" "$IMAGE_CHECKSUM" >> $name-bm-node.yaml
+        printf "\n  userData:" >> $name-bm-node.yaml
+        printf "\n    name: ""%s" "$name""-user-data" >> $name-bm-node.yaml
+        printf "\n    namespace: metal3\n" >> $name-bm-node.yaml
+        kubectl apply -f $name-bm-node.yaml -n metal3
+    done
+}
+
+function configure_nodes {
+    if [ ! -d $IRONIC_DATA_DIR ]; then
+        mkdir -p $IRONIC_DATA_DIR
+    fi
+
+    #make sure nodes.json file in /opt/ironic/ are configured
+    if [ ! -f $IRONIC_DATA_DIR/nodes.json ]; then
+        cp $PWD/nodes.json.sample $IRONIC_DATA_DIR/nodes.json
+    fi
+}
+
+function remove_bm_hosts {
+    while read -r name username password address; do
+        deprovision_compute_node $name
+    done
+}
+
+function apply_bm_hosts {
+    list_nodes | make_bm_hosts
+}
+
+function deprovision_all_hosts {
+    list_nodes | remove_bm_hosts
+}
+
+if [ "$1" == "launch" ]; then
+    launch_baremetal_operator
+    exit 0
+fi
+
+if [ "$1" == "deprovision" ]; then
+    deprovision_all_hosts
+    exit 0
+fi
+
+if [ "$1" == "provision" ]; then
+    apply_bm_hosts
+    exit 0
+fi
+
+echo "Usage: metal3.sh"
+echo "launch      - Launch the metal3 operator"
+echo "provision   - provision baremetal node as specified in common.sh"
+echo "deprovision - deprovision baremetal node as specified in common.sh"
+exit 1
+
+#Following code is tested for the offline mode
+#Will be intergrated for the offline mode for ICNi v.0.1.0 beta
+#create_ssh_key
+#create_userdata
+#set_compute_key
+#set_compute_ssh_config
--- env/metal3/02_configure.sh
+++ env/metal3/02_configure.sh
@@ -10,9 +10,9 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-function check_inteface_ip() {
-	local interface=$1
-	local ipaddr=$2
+function check_inteface_ip {
+    local interface=$1
+    local ipaddr=$2
 
     if [ ! $(ip addr show dev $interface) ]; then
         exit 1
@@ -24,138 +24,137 @@ function check_inteface_ip() {
     fi
 }
 
-function configure_dhcp_bridge() {
-	brctl addbr dhcp0
-	ip link set dhcp0 up
-	brctl addif dhcp0 $BS_DHCP_INTERFACE
-	ip addr add dev dhcp0 $BS_DHCP_INTERFACE_IP
+function configure_dhcp_bridge {
+    brctl addbr dhcp0
+    ip link set dhcp0 up
+    brctl addif dhcp0 $BS_DHCP_INTERFACE
+    ip addr add dev dhcp0 $BS_DHCP_INTERFACE_IP
 }
 
-function configure_ironic_bridge() {
-	brctl addbr provisioning
-	ip link set provisioning up
-	brctl addif provisioning $IRONIC_IPMI_INTERFACE
-	ip addr add dev provisioning 172.22.0.1/24
+function configure_ironic_bridge {
+    brctl addbr provisioning
+    ip link set provisioning up
+    brctl addif provisioning $IRONIC_INTERFACE
+    ip addr add dev provisioning 172.22.0.1/24
 }
 
-function configure_kubelet() {
-	swapoff -a
-	#Todo addition kubelet configuration
+function configure_kubelet {
+    swapoff -a
+    #Todo addition kubelet configuration
 }
 
-function configure_kubeadm() {
-	#Todo error handing
-	if [ "$1" == "offline" ]; then
-		for images in kube-apiserver kube-controller-manager kube-scheduler kube-proxy; do
-        	docker load --input $CONTAINER_IMAGES_DIR/$images.tar;
-		done
+function configure_kubeadm {
+    #Todo error handing
+    if [ "$1" == "offline" ]; then
+        for images in kube-apiserver kube-controller-manager kube-scheduler kube-proxy; do
+            docker load --input $CONTAINER_IMAGES_DIR/$images.tar;
+	done
 
-		docker load --input $CONTAINER_IMAGES_DIR/pause.tar
-		docker load --input $CONTAINER_IMAGES_DIR/etcd.tar
-		docker load --input $CONTAINER_IMAGES_DIR/coredns.tar
+	docker load --input $CONTAINER_IMAGES_DIR/pause.tar
+	docker load --input $CONTAINER_IMAGES_DIR/etcd.tar
+	docker load --input $CONTAINER_IMAGES_DIR/coredns.tar
         return
     fi
-	kubeadm config images pull --kubernetes-version=$KUBE_VERSION
+    kubeadm config images pull --kubernetes-version=$KUBE_VERSION
 }
 
-function configure_ironic_interfaces() {
-	#Todo later to change the CNI networking for podman networking
-	# Add firewall rules to ensure the IPA ramdisk can reach httpd, Ironic and the Inspector API on the host
-	if [ "$IRONIC_PROVISIONING_INTERFACE" ]; then
-		check_inteface_ip $IRONIC_PROVISIONING_INTERFACE $IRONIC_PROVISIONING_INTERFACE_IP	
-	else
-		exit 1
+function configure_ironic_interfaces {
+    #Todo later to change the CNI networking for podman networking
+    # Add firewall rules to ensure the IPA ramdisk can reach httpd, Ironic and the Inspector API on the host
+    if [ "$IRONIC_PROVISIONING_INTERFACE" ]; then
+        check_inteface_ip $IRONIC_PROVISIONING_INTERFACE $IRONIC_PROVISIONING_INTERFACE_IP	
+    else
+        exit 1
+    fi
 
-	fi
-
-	if [ "$IRONIC_IPMI_INTERFACE" ]; then
+    if [ "$IRONIC_IPMI_INTERFACE" ]; then
         check_inteface_ip $IRONIC_IPMI_INTERFACE $IRONIC_IPMI_INTERFACE_IP
     else
         exit 1
     fi
 
-	for port in 80 5050 6385 ; do
-    	if ! sudo iptables -C INPUT -i $IRONIC_PROVISIONING_INTERFACE -p tcp -m tcp --dport $port -j ACCEPT > /dev/null 2>&1; then
-        	sudo iptables -I INPUT -i $IRONIC_PROVISIONING_INTERFACE -p tcp -m tcp --dport $port -j ACCEPT
-    	fi
-	done
+    for port in 80 5050 6385 ; do
+        if ! sudo iptables -C INPUT -i $IRONIC_PROVISIONING_INTERFACE -p tcp -m tcp --dport $port -j ACCEPT > /dev/null 2>&1; then
+            sudo iptables -I INPUT -i $IRONIC_PROVISIONING_INTERFACE -p tcp -m tcp --dport $port -j ACCEPT
+        fi
+    done
 
-	# Allow ipmi to the bmc processes
-	if ! sudo iptables -C INPUT -i $IRONIC_IPMI_INTERFACE -p udp -m udp --dport 6230:6235 -j ACCEPT 2>/dev/null ; then
-    	sudo iptables -I INPUT -i $IRONIC_IPMI_INTERFACE -p udp -m udp --dport 6230:6235 -j ACCEPT
-	fi
+    # Allow ipmi to the bmc processes
+    if ! sudo iptables -C INPUT -i $IRONIC_IPMI_INTERFACE -p udp -m udp --dport 6230:6235 -j ACCEPT 2>/dev/null ; then
+        sudo iptables -I INPUT -i $IRONIC_IPMI_INTERFACE -p udp -m udp --dport 6230:6235 -j ACCEPT
+    fi
 
-	#Allow access to dhcp and tftp server for pxeboot
-	for port in 67 69 ; do
-    	if ! sudo iptables -C INPUT -i $IRONIC_PROVISIONING_INTERFACE -p udp --dport $port -j ACCEPT 2>/dev/null ; then
-        	sudo iptables -I INPUT -i $IRONIC_PROVISIONING_INTERFACE -p udp --dport $port -j ACCEPT
-    	fi
-	done
+    #Allow access to dhcp and tftp server for pxeboot
+    for port in 67 69 ; do
+        if ! sudo iptables -C INPUT -i $IRONIC_PROVISIONING_INTERFACE -p udp --dport $port -j ACCEPT 2>/dev/null ; then
+            sudo iptables -I INPUT -i $IRONIC_PROVISIONING_INTERFACE -p udp --dport $port -j ACCEPT
+        fi
+    done
 }
 
-function configure_ironic_offline() {
-	if [ ! -d $CONTAINER_IMAGES_DIR ] && [ ! -d $BUILD_DIR ]; then
-		exit 1	
+function configure_ironic_offline {
+    if [ ! -d $CONTAINER_IMAGES_DIR ] && [ ! -d $BUILD_DIR ]; then
+        exit 1	
+    fi
+
+    for image in ironic-inspector-image ironic-image podman-pause \
+	baremetal-operator socat; do
+	if [ ! -f "$CONTAINER_IMAGES_DIR/$image" ]; then
+	    exit 1
 	fi
+    done
 
-	for image in ironic-inspector-image ironic-image podman-pause \
-		baremetal-operator socat; do
-		if [ ! -f "$CONTAINER_IMAGES_DIR/$image" ]; then
-			exit 1
-		fi
-	done
+    if [ ! -f "$BUILD_DIR/ironic-python-agent.initramfs"] && [ ! -f \
+	"$BUILD_DIR/ironic-python-agent.kernel" ] && [ ! -f
+	"$BUILD_DIR/$BM_IMAGE" ]; then
+        exit 1
+    fi
 
-	if [ ! -f "$BUILD_DIR/ironic-python-agent.initramfs"] && [ ! -f \
-		"$BUILD_DIR/ironic-python-agent.kernel" ] && [ ! -f
-		"$BUILD_DIR/$BM_IMAGE" ]; then
-		exit 1
-	fi
+    podman load --input $CONTAINER_IMAGES_DIR/ironic-inspector-image.tar
+    podman load --input $CONTAINER_IMAGES_DIR/ironic-image.tar
+    podman load --input $CONTAINER_IMAGES_DIR/podman-pause.tar
 
-	podman load --input $CONTAINER_IMAGES_DIR/ironic-inspector-image.tar
-	podman load --input $CONTAINER_IMAGES_DIR/ironic-image.tar
-	podman load --input $CONTAINER_IMAGES_DIR/podman-pause.tar
+    docker load --input $CONTAINER_IMAGES_DIR/baremetal-operator.tar
+    docker load --input $CONTAINER_IMAGES_DIR/socat.tar
 
-	docker load --input $CONTAINER_IMAGES_DIR/baremetal-operator.tar
-	docker load --input $CONTAINER_IMAGES_DIR/socat.tar
+    mkdir -p "$IRONIC_DATA_DIR/html/images"
 
-	mkdir -p "$IRONIC_DATA_DIR/html/images"
-
-	cp $BUILD_DIR/ironic-python-agent.initramfs $IRONIC_DATA_DIR/html/images/
-	cp $BUILD_DIR/ironic-python-agent.kernel $IRONIC_DATA_DIR/html/images/
-	cp $BUILD_DIR/$BM_IMAGE $IRONIC_DATA_DIR/html/images/
-	md5sum $BUILD_DIR/$BM_IMAGE | awk '{print $1}' > $BUILD_DIR/${BM_IMAGE}.md5sum
+    cp $BUILD_DIR/ironic-python-agent.initramfs $IRONIC_DATA_DIR/html/images/
+    cp $BUILD_DIR/ironic-python-agent.kernel $IRONIC_DATA_DIR/html/images/
+    cp $BUILD_DIR/$BM_IMAGE $IRONIC_DATA_DIR/html/images/
+    md5sum $BUILD_DIR/$BM_IMAGE | awk '{print $1}' > $BUILD_DIR/${BM_IMAGE}.md5sum
 }
 
-function configure_ironic() {
-	if [ "$1" == "offline" ]; then
-		configure_ironic_offline
-		return
-	fi
+function configure_ironic {
+    if [ "$1" == "offline" ]; then
+        configure_ironic_offline
+	return
+    fi
 
-	podman pull $IRONIC_IMAGE
-	podman pull $IRONIC_INSPECTOR_IMAGE
+    podman pull $IRONIC_IMAGE
+    podman pull $IRONIC_INSPECTOR_IMAGE
 	
-	mkdir -p "$IRONIC_DATA_DIR/html/images"
-	pushd $IRONIC_DATA_DIR/html/images
+    mkdir -p "$IRONIC_DATA_DIR/html/images"
+    pushd $IRONIC_DATA_DIR/html/images
 	
-	if [ ! -f ironic-python-agent.initramfs ]; then
-		curl --insecure --compressed -L https://images.rdoproject.org/master/rdo_trunk/current-tripleo-rdo/ironic-python-agent.tar | tar -xf -
-	fi
+    if [ ! -f ironic-python-agent.initramfs ]; then
+	curl --insecure --compressed -L https://images.rdoproject.org/master/rdo_trunk/current-tripleo-rdo/ironic-python-agent.tar | tar -xf -
+    fi
 	
-	if [[ "$BM_IMAGE_URL" && "$BM_IMAGE" ]]; then
+    if [[ "$BM_IMAGE_URL" && "$BM_IMAGE" ]]; then
     	curl -o ${BM_IMAGE} --insecure --compressed -O -L ${BM_IMAGE_URL}
     	md5sum ${BM_IMAGE} | awk '{print $1}' > ${BM_IMAGE}.md5sum
-	fi
-	popd
+    fi
+    popd
 }
 
-function configure() {
-	configure_kubeadm $1
-	configure_kubelet
-	configure_ironic_interfaces
-	configure_ironic $1
-	configure_dhcp_bridge
-	configure_ironic_bridge
+function configure {
+    configure_kubeadm $1
+    configure_kubelet
+    configure_ironic $1
+    configure_dhcp_bridge
+    configure_ironic_bridge
+    configure_ironic_interfaces 
 }
 
 if [ "$1" == "-o" ]; then
