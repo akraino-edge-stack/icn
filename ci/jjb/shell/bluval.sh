4c50650e1eb96443dd427b49393ac5159481517d
Igor D.C <igordcard@gmail.com>
2020-06-06 20:18:53 +0000

    Always install/clean KUD on Bluval job
    This commit makes the Bluval job (icn-bluval-daily-master) always
    ensure KUD/kubespray is installed before running the validation steps.
    Additionally, it fully uninstalls KUD and purges all Docker images
    and Docker itself, so that the next run will be as clean as possible
    and also using the latest available KUD version and EMCO k8s code.
    
    Change-Id: Ie3e604315514b00b8a3933d8c69ca8fd3ff50458
    Signed-off-by: Igor D.C <igordcard@gmail.com>

--- ci/jjb/shell/bluval.sh
+++ ci/jjb/shell/bluval.sh
@@ -3,6 +3,42 @@ set -e
 set -o errexit
 set -o pipefail
 
+echo "[ICN] Downloading EMCO k8s"
+git clone "https://gerrit.onap.org/r/multicloud/k8s"
+cp ~/aio.sh k8s/kud/hosting_providers/baremetal/aio.sh
+cp ~/installer.sh k8s/kud/hosting_providers/vagrant/installer.sh
+
+echo "[ICN] Installing EMCO k8s"
+k8s/kud/hosting_providers/baremetal/aio.sh
+sleep 5
+
+echo "[ICN] Patching EMCO k8s security vulnerabilities"
+kubectl replace -f - << EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: pod-reader
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "false"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: system:public-info-viewer
+rules:
+- nonResourceURLs:
+  - /livez
+  - /readyz
+  - /healthz
+  verbs:
+  - get
+EOF
+kubectl replace -f - << EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: default
+automountServiceAccountToken: false
+EOF
+
 echo "[ICN] Downloading run_bluval.sh from upstream ci-management"
 wget --read-timeout=10 --timeout=10 --waitretry=10 -t 10 https://raw.githubusercontent.com/akraino-edge-stack/ci-management/master/jjb/shell/run_bluval.sh
 
