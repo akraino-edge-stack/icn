dedfb1a12ebc27f15f126c98364669b9ba33f4d0
Igor D.C <igordcard@gmail.com>
2020-06-06 20:18:53 +0000

    Always install/clean KUD on Bluval job
    This commit makes the Bluval job (icn-bluval-daily-master) always
    ensure KUD/kubespray is installed before running the validation steps.
    Additionally, it fully uninstalls KUD and purges all Docker images
    and Docker itself, so that the next run will be as clean as possible
    and also using the latest available KUD version and EMCO k8s code.
    
    Change-Id: Ie3e604315514b00b8a3933d8c69ca8fd3ff50458
    Signed-off-by: Igor D.C <igordcard@gmail.com>

--- ci/jjb/shell/bluval.sh
+++ ci/jjb/shell/bluval.sh
@@ -3,6 +3,14 @@ set -e
 set -o errexit
 set -o pipefail
 
+echo "[ICN] Downloading EMCO k8s"
+git clone "https://gerrit.onap.org/r/multicloud/k8s"
+cp ~/aio.sh k8s/kud/hosting_providers/baremetal/aio.sh
+cp ~/installer.sh k8s/kud/hosting_providers/vagrant/installer.sh
+
+echo "[ICN] Installing EMCO k8s"
+k8s/kud/hosting_providers/baremetal/aio.sh
+
 echo "[ICN] Downloading run_bluval.sh from upstream ci-management"
 wget --read-timeout=10 --timeout=10 --waitretry=10 -t 10 https://raw.githubusercontent.com/akraino-edge-stack/ci-management/master/jjb/shell/run_bluval.sh
 
