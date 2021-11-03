9ea25a47d73a7f6b5a9ce040fafa6d29a54e4649
Todd Malsbary <todd.malsbary@intel.com>
2021-11-03 10:45:04 -0700

    Add wait command to VM e2etest site
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>
    Change-Id: I06a273149ccbd10f22d38963e103505fe20d4169

--- deploy/site/vm/vm.sh
+++ deploy/site/vm/vm.sh
@@ -26,10 +26,36 @@ function clean {
     helm -n metal3 uninstall machines
 }
 
+function is_cluster_ready {
+    [[ $(kubectl -n metal3 get cluster e2etest -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}') == "True" ]]
+}
+
+function are_kustomizations_ready {
+    [[ $(kubectl --kubeconfig=${BUILDDIR}/e2etest-admin.conf get Kustomization -n flux-system -o jsonpath='{range .items[*]}{.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}' | grep -c -v True) == 0 ]]
+}
+
+function are_helmreleases_ready {
+    [[ $(kubectl --kubeconfig=${BUILDDIR}/e2etest-admin.conf get HelmRelease -n flux-system -o jsonpath='{range .items[*]}{.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}' | grep -c -v True) == 0 ]]
+}
+
+function wait_for_all_ready {
+    WAIT_FOR_INTERVAL=60s
+    wait_for is_cluster_ready
+    clusterctl -n metal3 get kubeconfig e2etest >${BUILDDIR}/e2etest-admin.conf
+    chmod 600 ${BUILDDIR}/e2etest-admin.conf
+    # TODO The following checks are not ideal: resources created by
+    # operators aren't detected here, but this is the best that can be
+    # currently done
+    WAIT_FOR_INTERVAL=30s
+    wait_for are_kustomizations_ready
+    wait_for are_helmreleases_ready
+}
+
 case $1 in
     "build") build ;;
     "clean") clean ;;
     "deploy") deploy ;;
+    "wait") wait_for_all_ready ;;
     *) cat <<EOF
 Usage: $(basename $0) COMMAND
 
@@ -37,6 +63,7 @@ Commands:
   build         - Build the site deployment values
   clean         - Remove the site
   deploy        - Deploy the site
+  wait          - Wait for the site to be ready
 EOF
        ;;
 esac
