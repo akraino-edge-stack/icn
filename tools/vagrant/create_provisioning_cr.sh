18c739dacd5ef0e9a7386f8410878a0ffccad6b4
Todd Malsbary <todd.malsbary@intel.com>
2021-12-02 16:17:57 -0800

    Flux install of compute cluster
    This includes SOPS encryption of BMC and cluster secrets.
    
    NOTE: Keys are included in this commit.  These are only intended to be
    used in this ICN virtual environment for testing and demonstration
    purposes.
    
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>
    Change-Id: I2c7b76ec5f2d125afb007d5a4b90a23924f8d305

--- tools/vagrant/create_provisioning_cr.sh
+++ tools/vagrant/create_provisioning_cr.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-num_machines=$1
-site=$2
-name_prefix=$3
+site=$1; shift
 
 provisioning_cr_path="cmd/bpa-operator/e2etest/test_bmh_provisioning_cr.yaml"
 
-name="${name_prefix}1"
+name=$1; shift
+ipmi_port=$1; shift
 provisioning_mac=$(virsh -c qemu:///system dumpxml "${site}-${name}" | xmlstarlet sel -t -v "//interface[source/@network='${site}-provisioning']/mac/@address")
 cat <<EOF >${provisioning_cr_path}
 apiVersion: bpa.akraino.org/v1alpha1
@@ -22,12 +21,13 @@ spec:
     - ${name}:
         mac-address: ${provisioning_mac}
 EOF
-if ((num_machines>1)); then
+if (("$#")); then
     cat <<EOF >>${provisioning_cr_path}
   workers:
 EOF
-    for ((i=2;i<=num_machines;++i)); do
-	name="${name_prefix}${i}"
+    while (("$#")); do
+	name=$1; shift
+	ipmi_port=$1; shift
 	provisioning_mac=$(virsh -c qemu:///system dumpxml "${site}-${name}" | xmlstarlet sel -t -v "//interface[source/@network='${site}-provisioning']/mac/@address")
 	cat <<EOF >>${provisioning_cr_path}
     - ${name}:
