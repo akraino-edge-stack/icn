e7e71a03c35316cfc4279f19bcaaca73fea339d0
Todd Malsbary <todd.malsbary@intel.com>
2022-01-24 14:42:29 -0800

    Remove BPA operator
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>
    Change-Id: Id7c9306378dd0fd252775bde8933251843bdc204

--- cmd/bpa-operator/pkg/apis/bpa/group.go
+++
@@ -1,6 +0,0 @@
-// Package bpa contains bpa API versions.
-//
-// This file ensures Go source parsers acknowledge the bpa package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package bpa
